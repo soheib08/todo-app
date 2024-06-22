80854d1ff7917180466eae46a26313873753e69b
soheib <soheib.mohammadi95@gmail.com>
2024-06-22 04:22:53 +0330

    add user jwt service

--- src/service/jwt/jwt.service.ts
+++ src/service/jwt/jwt.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { IJwtService } from 'src/domain/user/interface/jwt.service.interface';
 
 @Injectable()
 export class UserJwtService implements IJwtService {
-  constructor() {}
+  constructor(private jwtService: JwtService) {}
 
-  async verifyToken(token: string): Promise<boolean> {
-    return true;
+  async verifyToken(token: string) {
+    const payload = await this.jwtService.verifyAsync(token);
+    return payload;
   }
 
   async generateToken(payload: string): Promise<string> {
-    return '';
+    return await this.jwtService.signAsync(payload);
   }
 }
